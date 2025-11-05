2dd4c19795aaf346facf1c4d1a6294baa1c9138e
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2025-11-05 09:15:27 -0500

    firestore implemented

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useAuthStore from '../../stores/useAuthStore'
+import UserDao from '../../daos/UserDao'
 
 const Profile: React.FC = () => {
     const { user } = useAuthStore()
+
+    useEffect(() => {
+        if (user) {
+            const newUser = {
+                displayName: user?.displayName,
+                email: user?.email,
+                photoURL: user?.photoURL
+            }
+            UserDao.createUser(newUser);
+        }
+    }, [user])
 
     return (
         <div className="container-page">
